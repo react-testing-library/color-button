862a30875d3d15894413897c5c7f2d155190ab75
DonAdam2 <adam.morsi@yahoo.com>
2022-04-24 13:36:32 +0300

    test button functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import { hot } from 'react-hot-loader/root';
+import { useState } from 'react';
 
 const App = () => {
+  const [isRed, setIsRed] = useState(true);
   return (
     <div>
-      <button style={{ backgroundColor: 'red' }}>Change to blue</button>
+      <button
+        style={{ backgroundColor: isRed ? 'red' : 'blue' }}
+        onClick={() => setIsRed((prev) => !prev)}
+      >
+        Change to {isRed ? 'blue' : 'red'}
+      </button>
     </div>
   );
 };
--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,21 @@
-import { cleanup, render, screen } from '@testing-library/react';
+import { cleanup, render, screen, fireEvent } from '@testing-library/react';
 //component
 import App from './App';
 
 afterEach(cleanup);
 
 describe('App Component', () => {
-  beforeAll(() => {
+  beforeEach(() => {
     render(<App />);
   });
   test('button has correct initial color', () => {
     const button = screen.getByRole('button', { name: 'Change to blue' });
     expect(button).toHaveStyle({ backgroundColor: 'red' });
   });
-  test('button turns blue when clicked', () => {});
+  test('button turns blue when clicked', () => {
+    const button = screen.getByRole('button', { name: 'Change to blue' });
+    fireEvent.click(button);
+    expect(button).toHaveStyle({ backgroundColor: 'blue' });
+    expect(button).toHaveTextContent('Change to red');
+  });
 });
